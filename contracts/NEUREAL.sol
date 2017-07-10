c2b5e6e6f69a8afa1745ff9da6eb9782e2283b48
linagee <linagee@users.noreply.github.com>
2017-07-10 02:02:15 -0600

    approveAndCall not a problem, just debugging

--- contracts/NEUREAL.sol
+++ contracts/NEUREAL.sol
@@ -66,15 +66,15 @@ contract NeurealToken {
         return true;
     }
 
-//    /* Approve and then communicate the approved contract in a single tx */
-//    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-//        returns (bool success) {
-//        tokenRecipient spender = tokenRecipient(_spender);
-//        if (approve(_spender, _value)) {
-//            spender.receiveApproval(msg.sender, _value, this, _extraData);
-//            return true;
-//        }
-//    }        
+    /* Approve and then communicate the approved contract in a single tx */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
+        returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }        
 
     /* A contract attempts to get the coins */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {

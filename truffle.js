6f64cfab287cff0ca2a8bb0da5162153fb0cf1ea
linagee <linagee@gmail.com>
2017-07-11 15:54:30 -0600

    Added more truffle

--- truffle.js
+++ truffle.js
@@ -1,16 +1,17 @@
-const HDWalletProvider = require("truffle-hdwallet-provider")
-const fs = require("fs")
+/* jshint esversion: 6 */
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require("fs");
 
 // First read in the secrets.json to get our mnemonic
-let secrets
-let mnemonic
+let secrets;
+let mnemonic;
 if(fs.existsSync("secrets.json")) {
-  secrets = JSON.parse(fs.readFileSync("secrets.json", "utf8"))
-  mnemonic = secrets.mnemonic
+  secrets = JSON.parse(fs.readFileSync("secrets.json", "utf8"));
+  mnemonic = secrets.mnemonic;
 } else {
   console.log("No secrets.json found. If you are trying to publish EPM " +
-              "this will fail. Otherwise, you can ignore this message!")
-  mnemonic = ""
+              "this will fail. Otherwise, you can ignore this message!");
+  mnemonic = "";
 }
 
 module.exports = {
@@ -25,20 +26,22 @@ module.exports = {
       // from - default address to use for any transaction Truffle makes during migrations
     },
     morden: {
-      network_id: 2,
+      network_id: "2",
       host: "https://morden.infura.io",
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io"),
-      network_id: "3"
+      network_id: "3",
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io")
     },
     testrpc: {
-      network_id: "default"
+      network_id: "default",
+      host: "localhost",
+      port: "8545"
     },
     development: { //truffle test hardcodes the "test" network.
-      host: "localhost",
-      port: "8545",
       network_id: "default",
+      host: "localhost",
+      port: "8545"
     }
   }
 };

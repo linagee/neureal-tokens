8d6e1c213da17523f13e7d2f37ae161a4250a0a7
Wil Bown <wilbown@gmail.com>
2017-07-11 17:09:10 -0600

    oops

--- contracts/NECPToken.sol
+++ contracts/NECPToken.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.11;
 
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 contract owned {
     address public owner;
     function owned() {
